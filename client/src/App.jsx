4f997beec4234c6fde1a98b95286021999455441
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-10-22 21:46:27 -0500

    Moves Login Button And Functionality To Authentication Button In Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,49 +9,11 @@ import Profile from "./components/Profile.jsx";
 import NavBar from "./components/NavBar.jsx";
 
 function App() {
-  const { loginWithRedirect, logout, user, isAuthenticated, getAccessTokenSilently } = useAuth0();
   const navigate = useNavigate();
-
-  // If user already has a profile, go to Dashboard page. If not, go to Profile Setup page
-  useEffect(() => {
-    const checkProfile = async () => {
-      if (!isAuthenticated) return;
-
-      try {
-        const token = await getAccessTokenSilently({
-          audience: import.meta.env.VITE_AUTH0_AUDIENCE,
-        });
-
-        const response = await fetch("http://localhost:5000/api/profile", {
-          headers: { Authorization: `Bearer ${token}` },
-        });
-
-        if (response.status === 404) {
-          navigate("/profile-setup"); // New user → profile setup
-        } else if (response.ok) {
-          navigate("/dashboard"); // Returning user → dashboard
-        }
-      } catch (err) {
-        console.error("Error checking profile:", err);
-      }
-    };
-
-    checkProfile();
-  }, [isAuthenticated]);
 
   return (
     <div>
       <NavBar />
-      {/* If user isn't logged in, show Log In button. If user is logged in, show Log Out button and personalized welcome */}
-      {!isAuthenticated ? (
-        <button onClick={() => loginWithRedirect()}>Log In</button>
-      ) : (
-        <>
-          <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-            Log Out
-          </button>
-        </>
-      )}
       <Routes>
         <Route path="/" element={<HomePage />} />
       <Route path="/profile-setup" element={<ProfileSetup />} />
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
+import AuthenticationButton from "./AuthenticationButton";
 
 export default function NavBar() {
     return (
         <div>
             <nav>
                 <ul>
-                    <li>Login</li>
+                    <li><AuthenticationButton /></li>
                     <li><Link className='link' to="/profile"><div>Profile</div></Link></li>
                 </ul>
             </nav>
