bb32322a3b9da0ef7bef67738bc27146495052bd
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-10-23 21:35:49 -0500

    Adds Course List Page To NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -10,6 +10,7 @@ export default function NavBar() {
                     <li><Link className='link' to="/profile"><div>Profile</div></Link></li>
                     <li><Link className='link' to="/user-search">See All Users</Link></li>
                     <li><Link className='link' to="/create-course">Create Course</Link></li>
+                    <li><Link className='link' to="/course-list">Course List</Link></li>
                 </ul>
             </nav>
         </div>
