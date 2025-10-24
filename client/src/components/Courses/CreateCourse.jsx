6eb4616519da53f40f65e6fb3745a27127cccd99
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-10-23 21:20:19 -0500

    Adds Course Name Field To Create Course Form

--- client/src/components/Courses/CreateCourse.jsx
+++ client/src/components/Courses/CreateCourse.jsx
@@ -3,6 +3,10 @@ export default function CreateCourse() {
         <div>
             <h1>Create Course</h1>
             <form>
+                <div>
+                    <label>Course Name: </label>
+                    <input type="text" />
+                </div>
                 <button type="submit">Create Course</button>
             </form>
         </div>
