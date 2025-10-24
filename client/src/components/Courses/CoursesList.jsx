c51b568055a6cada35652501109fcf8b5dede4b9
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-10-23 21:19:38 -0500

    Adds Submit Button To Course

---
+++ client/src/components/Courses/CoursesList.jsx
@@ -0,0 +1,7 @@
+export default function CoursesList() {
+    return (
+        <div>
+            <h1>See All Courses</h1>
+        </div>
+    )
+}
---
+++ client/src/components/Courses/CreateCourse.jsx
@@ -0,0 +1,10 @@
+export default function CreateCourse() {
+    return (
+        <div>
+            <h1>Create Course</h1>
+            <form>
+                <button type="submit">Create Course</button>
+            </form>
+        </div>
+    )
+}
