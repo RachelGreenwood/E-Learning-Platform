250aac98e94b4b7a30e5ae84a094631ae0516d9a
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-10-25 11:47:51 -0500

    Fetches All Courses And Displays On CoursesList Page

--- client/src/components/Courses/CoursesList.jsx
+++ client/src/components/Courses/CoursesList.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
 export default function CoursesList() {
+    const [courses, setCourses] = useState([]);
+    const { getAccessTokenSilently } = useAuth0();
+
+    useEffect(() => {
+      const getCourses = async () => {
+        try {
+          const token = await getAccessTokenSilently();
+          const response = await fetch("http://localhost:5000/courses", {
+            headers: { Authorization: `Bearer ${token}` },
+          });
+          if (!response.ok) {
+            throw new Error("Failed to fetch courses");
+          }
+          const data = await response.json();
+          setCourses(data);
+        } catch (err) {
+          console.error("Error fetching courses:", err);
+        }
+      };
+  
+      getCourses();
+    }, [getAccessTokenSilently]);
+
     return (
         <div>
             <h1>See All Courses</h1>
+            {courses.length === 0 ? ( <p>No courses found. Check back later!</p>) : (
+                courses.map((course) => <p key={course.id}>{course.name}</p>)
+            )}
         </div>
     )
 }
