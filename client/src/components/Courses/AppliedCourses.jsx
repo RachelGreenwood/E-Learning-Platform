fe334f227808ee339dcd44192c3216ea23550a5e
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-10-26 11:43:11 -0500

    Maps Over Courses A User Has Applied To

--- client/src/components/Courses/AppliedCourses.jsx
+++ client/src/components/Courses/AppliedCourses.jsx
@@ -1,7 +1,52 @@
+import { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
 export default function AppliedCourses() {
-    return (
-        <div>
-            <h1>My Applied Courses</h1>
-        </div>
-    )
+  const [userCourses, setUserCourses] = useState([]);
+  const { getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    const fetchUserCourses = async () => {
+      try {
+        const token = await getAccessTokenSilently();
+        const res = await fetch("http://localhost:5000/user-courses", {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        if (!res.ok) throw new Error("Failed to fetch user courses");
+
+        const data = await res.json();
+        setUserCourses(data);
+      } catch (err) {
+        console.error("Error fetching user courses:", err);
+      }
+    };
+
+    fetchUserCourses();
+  }, [getAccessTokenSilently]);
+
+  return (
+    <div>
+      <h1>My Applied Courses</h1>
+      {userCourses.length === 0 ? (
+        <p>You haven’t applied to any courses yet.</p>
+      ) : (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Teacher</th>
+                </tr>
+            </thead>
+            <tbody>
+                {userCourses.map((course) => (
+                    <tr key={course.id}>
+                        <td>{course.course_name}</td>
+                        <td>Teacher: {course.teacher_name}</td>
+                    </tr>
+                ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
