b41231890be9d4926934adc3569867b14f325454
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-10-27 14:01:03 -0500

    Only Shows Courses User Has Created

--- client/src/components/Grades/MyCourses.jsx
+++ client/src/components/Grades/MyCourses.jsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
 export default function MyCourses() {
+    const [courses, setCourses] = useState([]);
+    const { getAccessTokenSilently, user } = useAuth0();
+    const apiUrl = import.meta.env.VITE_API_URL;
+
+    useEffect(() => {
+      const getCourses = async () => {
+        try {
+          const token = await getAccessTokenSilently();
+          const response = await fetch(`${apiUrl}/courses`, {
+            headers: { Authorization: `Bearer ${token}` },
+          });
+          if (!response.ok) {
+            throw new Error("Failed to fetch courses");
+          }
+          const data = await response.json();
+          setCourses(data);
+        } catch (err) {
+          console.error("Error fetching courses:", err);
+        }
+      };
+  
+      getCourses();
+    }, [getAccessTokenSilently]);
+
+    const instructorsCourses = courses.filter((course) => course.created_by === user.sub)
+
     return (
         <div>
             <h1>My Courses</h1>
+            {instructorsCourses.map((course) => (
+                <p key={course.id}>{course.name}</p>
+            ))}
         </div>
     )
 }
