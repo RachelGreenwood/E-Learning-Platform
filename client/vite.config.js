57b1f0387c4fcef8be832db1aec556f81479f74c
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-10-27 12:49:39 -0500

    Fixes Vite Config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { copyFileSync } from 'fs';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      // After build, copy _redirects into dist
-      plugins: [
-        {
-          name: 'copy-redirects',
-          closeBundle() {
-            copyFileSync(
-              resolve(__dirname, 'public/_redirects'),
-              resolve(__dirname, 'dist/_redirects')
-            );
-          },
-        },
-      ],
-    },
   },
 });
