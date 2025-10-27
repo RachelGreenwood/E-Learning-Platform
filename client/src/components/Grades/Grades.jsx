84f487a2651570926d15b0e446540b4d8e2e56d7
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-10-27 14:27:16 -0500

    Shows Student Details When Name Is Selected

--- client/src/components/Grades/Grades.jsx
+++ client/src/components/Grades/Grades.jsx
@@ -6,6 +6,7 @@ export default function Grades(props) {
     const [users, setUsers] = useState([]);
     const navigate = useNavigate();
     const apiUrl = import.meta.env.VITE_API_URL;
+    const [selectedStudent, setSelectedStudent] = useState(null);
 
     useEffect(() => {
         async function fetchUsers() {
@@ -23,17 +24,25 @@ export default function Grades(props) {
 
       const students = users.filter(user => user.role === "Student")
 
+      const handleSelectChange = (e) => {
+        const student = students.find(
+          (s) => s.username === e.target.value
+        );
+        setSelectedStudent(student);
+      };
+
     return (
         <div>
             <h1>Manage Grades</h1>
             {props.profile?.role === "Instructor" && (
-                <select>
+                <select onChange={handleSelectChange}>
+                  <option value="">Select a Student</option>
                 {students.map((user) => (
                     <option key={user.id}>{user.username}</option>
                 ))}
             </select>
             )}
-            <StudentDetails />
+            <StudentDetails student={selectedStudent} />
         </div>
     )
 }
--- client/src/components/Grades/StudentDetails.jsx
+++ client/src/components/Grades/StudentDetails.jsx
@@ -1,7 +1,14 @@
-export default function StudentDetails() {
+export default function StudentDetails({ student }) {
+      console.log("StudentDetails rendered with:", student);
+
+    if (!student) {
+    return <p>No student selected</p>;
+  }
+
     return (
         <div>
             <h1>Student Details</h1>
+            <p>{student.username}</p>
         </div>
     )
 }
